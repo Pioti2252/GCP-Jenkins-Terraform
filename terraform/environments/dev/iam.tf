47d9ece09910e646f29715d813b19c7ab40f3aa3
pioti <pioti2252@gmail.com>
2026-03-06 23:09:37 +0100

    update: add terraform and jenkins files

---
+++ terraform/environments/dev/iam.tf
@@ -0,0 +1,37 @@
+resource "google_service_account" "terraform_sa" {
+  account_id   = "terraform-sa"
+  display_name = "Terraform Service Account"
+}
+
+resource "google_service_account" "jenkins_gsa" {
+  account_id   = "jenkins-gsa"
+  display_name = "Jenkins Google Service Account"
+}
+
+locals {
+  terraform_roles = [
+    "roles/container.admin",
+    "roles/compute.admin",
+    "roles/artifactregistry.admin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/resourcemanager.projectIamAdmin"
+  ]
+
+  jenkins_roles = [
+    "roles/artifactregistry.writer"
+  ]
+}
+
+resource "google_project_iam_member" "terraform_sa_roles" {
+  for_each = toset(local.terraform_roles)
+  project  = var.project_id
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.terraform_sa.email}"
+}
+
+resource "google_project_iam_member" "jenkins_gsa_roles" {
+  for_each = toset(local.jenkins_roles)
+  project  = var.project_id
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.jenkins_gsa.email}"
+}
---
+++ terraform/environments/dev/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "first-jenkins-488416"
+region     = "europe-west1"
+zone       = "europe-west1-b"

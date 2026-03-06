78cf738eda69098482c648ffaf8084a695c360ef
pioti <pioti2252@gmail.com>
2026-03-07 00:50:19 +0100

    define the commit message for your changes. Lines starting

--- terraform/environments/dev/gke.tf
+++ terraform/environments/dev/gke.tf
@@ -36,6 +36,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     machine_type    = var.gke_machine_type
     disk_size_gb = var.gke_disk_size_gb
+    disk_type = "pd-standard"
     service_account = google_service_account.terraform_sa.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
